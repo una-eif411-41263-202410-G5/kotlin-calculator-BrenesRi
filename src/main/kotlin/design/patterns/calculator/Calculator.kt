ad3da01d7222834f4bcb332f37912c982ec487bb
BrenesRi <101229805+BrenesRi@users.noreply.github.com>
2024-02-23 19:10:49 -0600

    Añadidos Test Calculadora

---
+++ src/main/kotlin/design/patterns/calculator/Calculator.kt
@@ -0,0 +1,19 @@
+//Ricardo Brenes Curso Móviles
+package design.patterns.design.patterns.calculator
+class Calculator {
+    fun sumar(a: Double, b: Double): Double { //Obliga devolver double
+        return a + b
+    }
+
+    fun restar(a: Double, b: Double): Double {
+        return a - b
+    }
+
+    fun multiplicar(a: Double, b: Double): Double {
+        return a * b
+    }
+
+    fun dividir(a: Double, b: Double): Double {
+            return a / b
+    }
+}
---
+++ src/test/kotlin/design/patterns/calculator/CalculatorTest.kt
@@ -0,0 +1,42 @@
+package design.patterns.calculator
+
+import com.github.stefanbirkner.systemlambda.SystemLambda
+import design.patterns.design.patterns.singleton.MyClass
+import design.patterns.design.patterns.calculator.Calculator
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import java.io.ByteArrayOutputStream
+import java.io.PrintStream
+import kotlin.test.assertEquals
+
+class CalculatorTest {
+
+    @Test
+    fun testSumar() {
+        val calculadora = Calculator()
+        val resultado = calculadora.sumar(5.0, 3.0)
+        assertEquals(8.0, resultado, 0.0001)
+    }
+
+    @Test
+    fun testRestar() {
+        val calculadora = Calculator()
+        val resultado = calculadora.restar(5.0, 3.0)
+        assertEquals(2.0, resultado, 0.0001)
+    }
+
+    @Test
+    fun testMultiplicar() {
+        val calculadora = Calculator()
+        val resultado = calculadora.multiplicar(5.0, 3.0)
+        assertEquals(15.0, resultado, 0.0001)
+    }
+
+    @Test
+    fun testDividir() {
+        val calculadora = Calculator()
+        val resultadoExitoso = calculadora.dividir(6.0, 3.0)
+        assertEquals(2.0, resultadoExitoso, 0.0001)
+    }
+}
